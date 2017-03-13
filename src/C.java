148484e333094ea5f6d81ae0c2b7359a02ac954f
sweetweakness <sweetweakness1337>
2017-03-13 12:07:53 +0300

    2nd try

--- src/C.java
+++ src/C.java
@@ -4,7 +4,7 @@ public class C {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        double x=0;
+        double x=0; 
         for(int i=0;i<n;i++){
             x+=sc.nextDouble();
         }
