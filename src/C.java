5b267a133fd2c363a5c4ab050ff55ac066efe011
sweetweakness <sweetweakness1337>
2017-03-13 12:04:59 +0300

    2nd try

---
+++ src/C.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class C {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        double x=0;
+        for(int i=0;i<n;i++){
+            x+=sc.nextDouble();
+        }
+        x=x/n;
+        System.out.println(x);
+    }
+}
